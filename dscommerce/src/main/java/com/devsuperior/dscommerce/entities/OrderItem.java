f9e1ebaf21bc34393585df3ae353dec163875d71
leona <leonardovenanciocorreia@gmail.com>
2025-03-28 17:06:55 -0300

    ManyToMany with association class

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order); // ATRIBUINDO PARA A CLASSE PK
+        id.setProduct(product); // ATRIBUINDO PARA A CLASSE PK
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // feito na mão
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+    // fechando feito na mão
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
