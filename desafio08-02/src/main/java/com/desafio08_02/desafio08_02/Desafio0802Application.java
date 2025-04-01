59bf9fc703a5be29b8ed67fb9a937776c4b1563b
leona <leonardovenanciocorreia@gmail.com>
2025-04-01 18:46:51 -0300

    Initial commit

---
+++ desafio08-02/src/main/java/com/desafio08_02/desafio08_02/Desafio0802Application.java
@@ -0,0 +1,18 @@
+package com.desafio08_02.desafio08_02;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio0802Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio0802Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ desafio08-02/src/test/java/com/desafio08_02/desafio08_02/Desafio0802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio08_02.desafio08_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio0802ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
