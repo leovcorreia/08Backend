a16777aec99447f0cbc2bc785265658cf89bf21a
leona <leonardovenanciocorreia@gmail.com>
2025-04-15 19:58:31 -0300

    Customizing validation response

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.controllers.handlers;
 
 import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.dto.ValidationError;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -35,6 +38,24 @@ public class ControllerExceptionHandler {
                 e.getMessage(), // Mensagem da exceção ("Falha de integridade referencial")
                 request.getRequestURI() // Caminho da requisição (ex: "/products/1")
         );
+        return ResponseEntity.status(status).body(err);
+    }
+
+    // Quando requisição tem nome de produto vazio (" ")
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(
+                Instant.now(), // Quando o erro aconteceu
+                status.value(), // Código HTTP (422)
+                "Dados inválidos", // Mensagem da exceção
+                request.getRequestURI() // Caminho da requisição (ex: "/products/1")
+        );
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 }
