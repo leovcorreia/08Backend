8451ecb0bd0fb9987dcfd3d9dfcda895f70fa433
leona <leonardovenanciocorreia@gmail.com>
2025-04-16 08:42:11 -0300

    Initial commit

---
+++ desafio08-03-crud/src/main/java/com/desafio08_03_crud/clientes/ClientesApplication.java
@@ -0,0 +1,18 @@
+package com.desafio08_03_crud.clientes;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ desafio08-03-crud/src/test/java/com/desafio08_03_crud/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio08_03_crud.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
