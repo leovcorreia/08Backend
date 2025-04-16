daa9f8a6fcf3445f75b12bc4962c0beb9d50da43
leona <leonardovenanciocorreia@gmail.com>
2025-04-16 11:23:58 -0300

    READ by id done

--- desafio08-03-crud/src/main/java/com/desafio08_03_crud/clientes/services/ClientService.java
+++ desafio08-03-crud/src/main/java/com/desafio08_03_crud/clientes/services/ClientService.java
@@ -1,13 +1,30 @@
 package com.desafio08_03_crud.clientes.services;
 
+import com.desafio08_03_crud.clientes.dto.ClientDTO;
+import com.desafio08_03_crud.clientes.entities.Client;
 import com.desafio08_03_crud.clientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
+    /*@Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }*/
 
 }
